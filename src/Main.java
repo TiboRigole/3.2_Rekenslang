aeac0b7c3f1a98e0965784f6a2328ae20ea7fc1d
Tibo Rigole <tibo.rigole@gmail.com>
2018-07-25 16:06:07 +0200

    generator van de verschillende mogelijkheden gevonden

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,47 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] sequence = new int [3];
+		sequence[0] = 1;
+		sequence[1] = 2;
+		sequence[2] = 3;
+		
+		permute(sequence, 0);
+		
+		
+		
 	}
-
+	
+	//https://www.sanfoundry.com/java-program-generate-all-possible-combinations-given-list-numbers/
+	static void permute(int[] a, int k) 
+    {
+        if (k == a.length) 
+        {
+            for (int i = 0; i < a.length; i++) 
+            {
+                System.out.print(a[i]);
+            }
+            System.out.println();
+        } 
+        else 
+        {
+            for (int i = k; i < a.length; i++) 
+            {
+                int temp = a[k];
+                a[k] = a[i];
+                a[i] = temp;
+ 
+                permute(a, k + 1);
+ 
+                temp = a[k];
+                a[k] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+	
+	
 }
+
+
